531e131163d6841f30884113d534021f6b7a3c00
Brady Pratt <bpratt@redhat.com>
2024-01-23 10:37:13 -0600

    try processing the local template with vars from the makefile
    Signed-off-by: Brady Pratt <bpratt@redhat.com>

--- hack/translate-operator-template.sh
+++ hack/translate-operator-template.sh
@@ -12,9 +12,29 @@ echo "${OPERATOR_VERSION}"
 # default path to olm-artifacts-template.yaml file
 _TEMPLATE_FILE=../hack/olm-registry/olm-artifacts-template.yaml
 
-cat ${_TEMPLATE_FILE}
+# TODO: determine this
+_OPERATOR_OLM_CHANNEL=staging
+_OPERATOR_OLM_REGISTRY_IMAGE_TAG="${_OPERATOR_OLM_CHANNEL}-latest"
+
+# look up the digest for the new registry image
+_OPERATOR_OLM_REGISTRY_IMAGE_DIGEST=$(skopeo inspect \
+    --format '{{.Digest}}' \
+    docker://"${OPERATOR_OLM_REGISTRY_IMAGE}":"${_OPERATOR_OLM_REGISTRY_IMAGE_TAG}" \
+    | tr -d "\r")
 
 log "Process template with parameters..."
+_PROCESSED_TEMPLATE=$(oc process \
+	--local \
+	--output=yaml \
+	--ignore-unknown-parameters \
+	--filename \
+	"${_TEMPLATE_FILE}" \
+	CHANNEL="${_OPERATOR_OLM_CHANNEL}" \
+	IMAGE_TAG="${_OPERATOR_OLM_REGISTRY_IMAGE_TAG}" \
+	REGISTRY_IMG="${OPERATOR_OLM_REGISTRY_IMAGE}" \
+	IMAGE_DIGEST="${_OPERATOR_OLM_REGISTRY_IMAGE_DIGEST}")
+
+echo "${_PROCESSED_TEMPLATE}"
 
 log "Appending 'package-operator.run/phase' to every object and writing to ${_OUTDIR} ..."
 
