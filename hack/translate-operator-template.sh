a4442897e86daf8fe12263d8a437e6643ae8b0c4
Brady Pratt <bpratt@redhat.com>
2024-01-24 13:34:43 -0600

    pko bundle and shfmt
    Signed-off-by: Brady Pratt <bpratt@redhat.com>

--- hack/translate-operator-template.sh
+++ hack/translate-operator-template.sh
@@ -6,10 +6,14 @@ function log() {
 	echo "[bundle] ${1}"
 }
 
-# assert required vars are being passed through
+# TODO: assert required tools are in path
+# skopeo, oc, yq, kubectl-package
+# TODO: assert required vars are being passed through
+# $OPERATOR_NAME $OPERATOR_VERSION $OPERATOR_OLM_REGISTRY_IMAGE
 
 # default path to olm-artifacts-template.yaml file
 _TEMPLATE_FILE=../hack/olm-registry/olm-artifacts-template.yaml
+_BUNDLE_REGISTRY=ghcr.io/jbpratt/managed-openshift/release-bundle
 
 _OUTDIR=resources/${OPERATOR_NAME}
 rm -rf "${_OUTDIR}" && mkdir -p "${_OUTDIR}"
@@ -20,9 +24,9 @@ _OPERATOR_OLM_REGISTRY_IMAGE_TAG="${_OPERATOR_OLM_CHANNEL}-latest"
 
 # look up the digest for the new registry image
 _OPERATOR_OLM_REGISTRY_IMAGE_DIGEST=$(skopeo inspect \
-    --format '{{.Digest}}' \
-    docker://"${OPERATOR_OLM_REGISTRY_IMAGE}":"${_OPERATOR_OLM_REGISTRY_IMAGE_TAG}" \
-    | tr -d "\r")
+	--format '{{.Digest}}' \
+	docker://"${OPERATOR_OLM_REGISTRY_IMAGE}":"${_OPERATOR_OLM_REGISTRY_IMAGE_TAG}" |
+	tr -d "\r")
 
 log "Process template with parameters..."
 _PROCESSED_TEMPLATE=$(oc process \
@@ -60,3 +64,12 @@ fi
 
 log "Committing changes..."
 git commit --quiet --message "${OPERATOR_NAME}: ${OPERATOR_VERSION}"
+# git push
+
+_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+_COMMIT=$(git rev-parse --short HEAD)
+_BUILD_NUMBER=$(git rev-list --count HEAD)
+_BUNDLE_IMAGE_NAME=${_BUNDLE_REGISTRY}:${_BRANCH/#release-/}.${_BUILD_NUMBER}-${_COMMIT}
+
+log "Building and pushing package ${_BUNDLE_IMAGE_NAME} ..."
+kubectl package build --push --tag "${_BUNDLE_IMAGE_NAME}" ./resources
