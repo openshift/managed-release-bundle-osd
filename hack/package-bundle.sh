96677f83bc50fc6dc91d698d8dde377cb67e116a
Brady Pratt <bpratt@redhat.com>
2024-04-09 04:55:13 -0500

    SDCICD-1273: parameterize script for HCP
    allow for passing in the directory and registry image name to rebuild
    the new rosa-hcp package
    
    Signed-off-by: Brady Pratt <bpratt@redhat.com>

--- hack/package-bundle.sh
+++ hack/package-bundle.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+_DIRECTORY="${1:-./resources}"
+_BUNDLE_REGISTRY="${2:-quay.io/app-sre/managed-release-bundle}"
+
 _KUBECTL_PACAKGE_VERSION=v1.10.0
 KUBECTL_PACKAGE=kubectl-package
 if ! command -v ${KUBECTL_PACKAGE} &>/dev/null; then
@@ -17,12 +20,10 @@ if [[ ${QUAY_DOCKER_CONFIG_JSON} ]]; then
 	export DOCKER_CONFIG=.docker
 fi
 
-_BUNDLE_REGISTRY="${BUNDLE_REGISTRY:-quay.io/app-sre/managed-release-bundle}"
-
 _BRANCH=$(git branch --remote --contains HEAD | cut -d / -f 2)
 _COMMIT=$(git rev-parse --short HEAD)
 _BUILD_NUMBER=$(git rev-list --count HEAD)
 _BUNDLE_IMAGE_NAME=${_BUNDLE_REGISTRY}:${_BRANCH/#release-/}.${_BUILD_NUMBER}-${_COMMIT}
 
 echo "Building and pushing package ${_BUNDLE_IMAGE_NAME} ..."
-${KUBECTL_PACKAGE} build --push --tag "${_BUNDLE_IMAGE_NAME}" ./resources
+${KUBECTL_PACKAGE} build --push --tag "${_BUNDLE_IMAGE_NAME}" "${_DIRECTORY}"
